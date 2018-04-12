7ac71934ddbf0f1b045725650a0bd37dbf1a6334
Dror Shimoni <drorshim@gmail.com>
2018-04-12 17:37:30 +0300

    function first stage

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/test', function(req, res, next) {
+//    console.log(req.body);
+//    console.log("data we got = ", req.rawBody);
+
+    height = 2;
+    width = 3;
+    data = "[182,23,234][172,23,234][12,263,234]" +
+           "[2,3,34][1,2,24][62,43,284]";
+    parsed_light = read_led_data(height,width,data);
+    res.json(parsed_light);
+});
+
+function read_led_data(height, width, data){
+    retval = [];
+    data_array = data.split(']');
+    for (var i=0;i<height;i++){
+        for (var j=0;j<width;j++){
+            curr_string = data_array[j+i*width];
+
+            // light as [R,B,G]
+            curr_light = curr_string.slice(1).split(',');
+
+            console.log(curr_light,i,j);
+            retval.push(curr_light);
+            //Update the LEd Value
+        }
+    }
+
+    return retval;
+}
+
+
 
 app.listen(3101, () => console.log('Example app listening on port 3101!'))
